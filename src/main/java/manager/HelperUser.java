4778ea06ce40652b215ca63479ef2509e88f32f1
NickV1199 <volkovnikolai@protonmail.com>
2026-04-30 20:33:25 +0300

    Create project

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -2,6 +2,7 @@ package manager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class HelperUser extends  HelperBase{
 
@@ -11,8 +12,31 @@ public class HelperUser extends  HelperBase{
 
     public void openLoginRegistrationForm(){
         //wd.findElement(By.cssSelector("a[href ='/login']"));
-        wd.findElement(By.xpath("//a[text()='LOGIN']"));
+//        WebElement loginTab = wd.findElement(By.xpath("//a[text()='LOGIN']"));
+//        loginTab.click();
+        click(By.cssSelector("a[href ='/login']"));
 
 
     }
+
+    public void fillLoginRegistrationForm(String email, String password){
+//        WebElement emailInput = wd.findElement(By.name("email"));
+//        emailInput.click();
+//        emailInput.clear();
+//        emailInput.sendKeys(email);
+        type(By.name("email"), email);
+
+//        WebElement passwordInput = wd.findElement(By.xpath("//input[@placeholder = 'Password']"));
+//        passwordInput.click();
+//        passwordInput.clear();
+//        passwordInput.sendKeys(password);
+        type(By.xpath("//input[@placeholder = 'Password']"), password);
+
+
+    }
+
+    public void submitLogin(){
+        click(By.xpath("//button[text(")]"));
+    }
+
 }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -3,13 +3,21 @@ package tests;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
-public class LoginTest extends TestBase{
+public class LoginTest extends TestBase {
 
 
     @Test
-    public void test(){
+    public void test() {
 
     }
 
+    @Test
+    public void loginSuccess() {
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("margo@gmail.com", "Mmar123456$");
+        app.getHelperUser().submitLogin();
+
+        //Assert
+    }
 
 }
