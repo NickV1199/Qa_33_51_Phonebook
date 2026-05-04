4709afa0031d8d731cac43f101a7f03101b61108
NickV1199 <volkovnikolai@protonmail.com>
2026-05-04 19:03:07 +0300

    added loginsuccessModel and Beforemethod

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -2,15 +2,28 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTest extends TestBase {
 
 
-    @Test
-    public void test() {
+//    @Test
+//    public void test() {
+//
+//    }
+
+    @BeforeMethod
+    public void preCondition(){
+
+        //If button signe out present ---> logout
+
+        if(app.getHelperUser().isLogged()){
+            app.getHelperUser().logout();
+        }
 
     }
+
 
     @Test
     public void loginSuccess() {
@@ -29,7 +42,21 @@ public class LoginTest extends TestBase {
     }
 
 
+    @Test
+    public void loginSuccessModel() {
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("margo@gmail.com", "Mmar123456$");
+        app.getHelperUser().submitLogin();
 
+        //Assert
+//        Assert.assertEquals();
+//        Assert.assertNotEquals();
+//        Assert.assertTrue();
+//        Assert.assertFalse();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
+
+    }
 
 
 
